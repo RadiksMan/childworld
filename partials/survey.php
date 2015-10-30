3ea4b3cab1d1014cea03f4c0f42b1eba293145ee
ArtGarm <artem.garmatyk@gmail.com>
2015-10-30 16:45:42 +0200

    add fonts

--- partials/survey.php
+++ partials/survey.php
@@ -1,36 +1,31 @@
 <div class="container survey-content">
-    <div class="row survey-header">
-        <div class="col-sm-12">
+    <div class="row survey-header-wrap">
+        <div class="col-sm-12 survey-header">
             <p>Съемка в детском саду</p>
         </div>
     </div>
-    <div class="row  children-fancybox">
-        <div class="col-sm-3">
-            <a href="">
-                <img src="images/child-1.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-sm-3">
-            <a href="">
-                <img src="images/child-2.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-sm-3">
-            <a href="">
-                <img src="images/child-3.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-sm-3">
-            <a href="">
-                <img src="images/child-4.jpg" alt="">
-            </a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-6">
+
+    <div class="row children-box-wrap">
+        <div class="col-sm-6 children-box-coll">
+            <div class="box-imges">
+                <a href="">
+                    <img src="images/child-1.jpg" alt="">
+                </a>
+                <a href="">
+                    <img src="images/child-2.jpg" alt="">
+                </a>
+            </div>
             <p>Задача организации, в особенности же реализация намеченных плановых заданий позволяет оценить значение дальнейших направлений развития. Значимость этих проблем настолько очевидна, что постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет оценить значение модели развития </p>
         </div>
-        <div class="col-sm-6">
+        <div class="col-sm-6 children-box-coll">
+            <div class="box-imges">
+                <a href="">
+                    <img src="images/child-3.jpg" alt="">
+                </a>
+                <a href="">
+                    <img src="images/child-4.jpg" alt="">
+                </a>
+            </div>
             <p>Значимость этих проблем настолько очевидна, что начало повседневной работы по формированию позиции способствует подготовки и реализации модели развития. Товарищи! новая модель организационной деятельности в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям.</p>
         </div>
     </div>
