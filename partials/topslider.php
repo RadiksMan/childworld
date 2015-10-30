fb5bb52e18412a99310320a54deff222f98ebb83
Dmitry <dmitry1940@mail.ru>
2015-10-30 16:31:17 +0200

    added markup to topSlider + container width

---
+++ partials/topslider.php
@@ -0,0 +1,33 @@
+<div class="topSlider">
+    <div class="topSlider-title">Профессиональная фотосъёмка в детских садах и школах</div>
+    <div class="topSlider-main">
+        <div class="topSlider-item">
+            <div class="topSlider-img">
+                <img src="images/topSlider-img.jpg" alt="" />
+            </div>
+            <div class="topSlider-description">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-4">
+                            Товарищи! консультация с широким активом позволяет оценить значение дальнейших направлений развития. Значимость этих проблем настолько очевидна, что рамки и место обучения кадров.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="topSlider-item">
+            <div class="topSlider-img">
+                <img src="images/topSlider-img.jpg" alt="" />
+            </div>
+            <div class="topSlider-description">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-4">
+                            1Товарищи! консультация с широким активом позволяет оценить значение дальнейших направлений развития. Значимость этих проблем настолько очевидна, что рамки и место обучения кадров.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
