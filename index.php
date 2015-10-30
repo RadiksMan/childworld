052b242a382bcdfa711a79d8f0d7573bfe522d32
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-30 17:37:23 +0200

    no message

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 	<div class="main">
-        <?partial('topslider')?>
-        <?partial('quastion')?>
-        <?partial('quastionform')?>
-        <?partial('survey')?>
-        <?partial('decoration')?>
-        <?partial('coveralbum')?>
-        <?partial('grafic')?>
-        <?partial('school')?>
-        <?partial('school')?>
-        <?partial('quality')?>
-        <?partial('sertification')?>
-        <?partial('portfolio')?>
+                <?partial('topslider')?>
+                <?partial('quastion')?>
+                <?partial('quastionform')?>
+                <?partial('survey')?>
+                <?partial('decoration')?>
+                <?partial('coveralbum')?>
+                <?partial('grafic')?>
+                <?partial('school')?>
+                <?partial('school')?>
+                <?partial('quality')?>
+                <?partial('sertification')?>
+                <?partial('portfolio')?>
 	</div>
