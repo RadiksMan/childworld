99c042af3d70e6dc4f0dfcb64d5ad6e1469abacd
Radiks <radion91@gmail.com>
2015-10-30 16:47:55 +0200

    footer 30%

--- footer.php
+++ footer.php
@@ -1,8 +1,30 @@
 	<div class="footer_placeholder"></div>
 	<footer class="footer">
-		<div class="mbox">
-			
-		</div>
+        <div class="if_quastion">
+            <div class="container">
+                <div class="if_quastion_wrap">
+                    <div class="row">
+                        <div class="col-sm-7">
+                            <div class="if_quastion_title">
+                                <h5>Возникли вопросы?</h5>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-sm-offset-1">
+                            <div class="if_quastion_right">
+                                <p>
+                                    <span>звоните </span>
+                                    <a href="tel:849588649645"><span><b>8 (495) 886-49-645</b></span></a>
+                                </p>
+                                <p>
+                                    <span>пишите</span>
+                                    <a href="mailto:foto@mig-detstva.ru"><span><i>foto@mig-detstva.ru</i></span></a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 	</footer>
 </body>
 </html>
