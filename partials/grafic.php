ae1a45362102ea0dde24a6452b4deeeab4b386d7
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-11-02 14:36:29 +0200

    no message

--- partials/grafic.php
+++ partials/grafic.php
@@ -9,38 +9,34 @@
 						</div>
 					</div>
 					<div class="grafic-body">
-						<div class="grafic-desc">
-							<div class="col-sm-5">
-								<p>Сегодня <span>1 октября.</span></p>
-								<p>В настоящее время формируется график съёмок на следующий месяц.</p>
-								<p>Позвоните нашим ассистентам для резервирования даты фотосессии или оставьте заявку на обратный звонок.</p>
-							</div>
+						<div class="grafic-desc col-sm-5">
+							<p>Сегодня <span>1 октября.</span></p>
+							<p>В настоящее время формируется график съёмок на следующий месяц.</p>
+							<p>Позвоните нашим ассистентам для резервирования даты фотосессии или оставьте заявку на обратный звонок.</p>
 						</div>
-						<div class="grafic-form cfix">
-							<div class="col-sm-7">
-								<div class="grafic-form-wrap">
-									<p>8 (926) 357-32-88 <span>Валерия</span></p>
-									<p>8 (916) 409-52-00 <span>Елена Васильевна</span></p>
-									<div class="row">
-										<div class="form-wrap cfix">
-											<form method="POST" action="" id="grafic-form">
-												<div class="input-wrap">
-													<div class="col-sm-4">
-														<input type="text" name="name" placeholder="Имя:" required="required"/>
-													</div>
+						<div class="grafic-form col-sm-7">
+							<div class="grafic-form-wrap">
+								<p>8 (926) 357-32-88 <span>Валерия</span></p>
+								<p>8 (916) 409-52-00 <span>Елена Васильевна</span></p>
+								<div class="row">
+									<div class="form-wrap cfix">
+										<form method="POST" action="" id="grafic-form">
+											<div class="input-wrap">
+												<div class="col-sm-4">
+													<input type="text" name="name" placeholder="Имя:" required="required"/>
 												</div>
-												<div class="input-wrap phone">
-													<div class="col-sm-4">
-														<input type="text" name="phone" placeholder="Телефон:" required="required"/>
-													</div>
+											</div>
+											<div class="input-wrap phone">
+												<div class="col-sm-4">
+													<input type="text" name="phone" placeholder="Телефон:" required="required"/>
 												</div>
-												<div class="input-wrap-submit">
-													<div class="col-sm-4">
-														<input type="submit" name="submit" value="Обратный звонок"/>
-													</div>
+											</div>
+											<div class="input-wrap-submit">
+												<div class="col-sm-4">
+													<input type="submit" name="submit" value="Обратный звонок"/>
 												</div>
-											</form>
-										</div>
+											</div>
+										</form>
 									</div>
 								</div>
 							</div>
