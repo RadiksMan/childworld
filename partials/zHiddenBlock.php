727a783c7d3503ec1e7e1186beefef8e65f7b898
ArtGarm <artem.garmatyk@gmail.com>
2015-11-12 14:47:00 +0200

    cfg

--- partials/zHiddenBlock.php
+++ partials/zHiddenBlock.php
@@ -1,10 +1,137 @@
 <div class="zHiddenBlock">
-	<div id="call_success">
-        <div class="call-success">
-             <div class="call_success">
-                 <div class="call-title">Спасибо за заявку!</div>
-                 <div class="call-subtitle">Мы свяжемся с Вами в ближайшее время</div>
-             </div>
+    <div id="wrappp">
+        <div id="call-popup">
+            <div class="call-popup">
+                <p>Заказать обратный звонок</p>
+                <form class="callback-form">
+                    <div class="form-wrap-for-rows">
+                        <div class="form-wrap-row form_row">
+                            <div class="form-wrap-value">
+                                <div class="form-wrap-input form_input">
+                                     <input type="text" name="name" placeholder="Ваше имя" required="required"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-wrap-row form_row ">
+                            <div class="form-wrap-value">
+                                <div class="form-wrap-input form_input inputNum">
+                                    <input type="text" name="phone" placeholder="Ваш контакный телефон" required="required"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-wrap-row form_row">
+                            <div class="form-wrap-value ">
+                                <div class="form-wrap-select form_input">
+                                    <select class="select-wrap time-pick " name="select-time" required="required">
+                                        <option selected="selected" disabled selected>Удобное время для звонка</option>
+                                        <option value="time-now"> <span>Позвонить сейчас</span></option>
+                                        <option value="time-15"> <span>Через 15 минут</span></option>
+                                        <option value="time-30"> <span>Через 30 минут</span></option>
+                                        <option value="time-1h"> <span>Через 1 час </span></option>
+                                        <option value="time-choose"> <span>Указать время </span></option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-wrap-row form_row add-row-for-time ">
+                            <div class="form-wrap-value">
+                                <div class="form-wrap-input form_input ">
+                                    <input type="text" class="addfild" name="select-time-often" placeholder="Введите удобное время" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-wrap-row ">
+                            <div class="form-wrap-value submit">
+                                <div class="form-wrap-input">
+                                    <button class="form-button" type="submit">
+                                        <span class="form-button-hover"></span>
+                                        <span class="form-button-text">Заказать звонок</span>
+                                    </button>
+                                </div>
+                            </div>
+                            <a class="form-button-close" href="#">
+                                <div class="form-wrap-value no-submit">
+                                    <div class="form-wrap-input">Отмена</div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </form>
+                <div class="call-success">
+                    <div class="call_success">
+                        <div class="call-title">Спасибо. Мы позвоним Вам в указанное время.</div>
+                     </div>
+                </div>
+
+            </div>
+
+
         </div>
     </div>
+
+
+
+
+    <div id="letter-online">
+        <div class="letter-online-popup">
+            <p>ОН-ЛАЙН ЗАЯВКА</p>
+            <h1>Мы предлагаем конкурентные цены и непревзойденный уровень обслуживания при оказании всех видов консультационных услуг. Для получения дополнительной информации о наших услугах свяжитесь с нами используя наши контактные данные в разделе Контакты либо заполните форму ниже и мы в кратчайшие сроки свяжемся с Вами:</h1>
+            <form  class="letter-online-form">
+                <div class="atribute cfix">
+                    <div class="convert-left"><p>Ваше имя</p></div>
+                    <div class="convert-right form_input">
+                        <input class="atribute-caption atrr-name" type="text" name="name" placeholder="" required="required">
+                    </div>
+                </div>
+                <div class="atribute cfix">
+                    <div class="convert-left"><p>Ваша должность в компании</p></div>
+                    <div class="convert-right form_input">
+                        <input class="atribute-caption atrr-dolzh" type="text" name="placeIn"  required="required">
+                    </div>
+                </div>
+                <div class="atribute cfix">
+                    <div class="convert-left"><p>Ваша компания</p></div>
+                    <div class="convert-right form_input">
+                        <input class="atribute-caption atrr-company" type="text" name="company"  required="required">
+                    </div>
+                </div>
+                <div class="atribute cfix">
+                    <div class="convert-left"><p>Ваш телефон</p></div>
+                    <div class="convert-right form_input inputNum">
+                        <input class="atribute-caption atrr-phone" type="text" name="phone" required="required">
+                    </div>
+                </div>
+                <div class="atribute cfix">
+                    <div class="convert-left"><p>Адрес электронной почты</p></div>
+                    <div class="convert-right form_input">
+                        <input class="atribute-caption atrr-email" type="email" name="email" required="required">
+                    </div>
+                </div>
+                <div class="atribute cfix">
+                    <div class="convert-left"><p>Описание запроса</p></div>
+                    <div class="convert-right form_input">
+                        <textarea class="atribute-caption atrr-texter" rows="10" cols="50"></textarea>
+                    </div>
+                </div>
+
+                <div class="letter-online-wrap-submit form_input">
+                    <button type="submit" class="submit-form"><span> >Отправить</span></button>
+                </div>
+
+            </form>
+
+            <div class="call-success">
+                <div class="call_success">
+                    <div class="call-title">Мы получили Ваш запрос и ответим Вам в ближайшее время.</div>
+                 </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 </div>
