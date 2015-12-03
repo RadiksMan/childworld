06bf954449ed2af5b7de81291d21e268564e274c
Dmitry <dmitry1940@mail.ru>
2015-12-03 17:24:50 +0200

    added icons

--- partials/quastionform.php
+++ partials/quastionform.php
@@ -33,6 +33,33 @@
                             <span>Елена Васильевна</span> <a href="tel:89164095200"><span>8 (916) 409-52-00</span></a>
                             <p>ассистент фотографа, специалист по работе с детьми</p>
                         </div>
+                        <div class="vatsap-icon-main">
+                            <a href="#" class="vatsap-icon">
+                                <span>
+                                    <img src="images/vatsap-icon.png" alt="" />
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+                    <div class="socials-wrap">
+                        <div class="social-item">
+                            <a href="https://vk.com/club107011596" class="vk-icon">
+                                <span>
+                                    <img src="images/vk-icon.png" alt="" />
+                                </span>
+                            </a>
+                        </div>
+                        <div class="social-item">
+                            <a href="https://www.instagram.com/mig_detstva/" class="instagram-icon">
+                                <span>
+                                    <img src="images/instagram-icon.png" alt="" />
+                                </span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
